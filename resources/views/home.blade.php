29d10d0482e59f6d835b0b0bc03e6ddfb11cb00e
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-02-17 14:59:32 +0100

    Creata Migration, Model, Controller, e Route annesse per le Crud

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'homepage')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Hello World!!</h1>
+            </div>
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home')}}">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="{{ route('home')}}">Home</a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
